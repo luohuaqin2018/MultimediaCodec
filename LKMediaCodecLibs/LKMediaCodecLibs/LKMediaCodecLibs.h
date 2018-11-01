aafa461f879f10a8923debbe275aa6c45c0b4fb5
hellboy9257 <luoxingrui2012@aliyun.com>
2018-11-01 11:27:40 +0800

    add files

---
+++ LKMediaCodecLibs/LKMediaCodecLibs/LKMediaCodecLibs.h
@@ -0,0 +1,19 @@
+//
+//  LKMediaCodecLibs.h
+//  LKMediaCodecLibs
+//
+//  Created by Macrovideo on 2018/10/26.
+//  Copyright © 2018 Macrovideo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LKMediaCodecLibs.
+FOUNDATION_EXPORT double LKMediaCodecLibsVersionNumber;
+
+//! Project version string for LKMediaCodecLibs.
+FOUNDATION_EXPORT const unsigned char LKMediaCodecLibsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LKMediaCodecLibs/PublicHeader.h>
+
+
